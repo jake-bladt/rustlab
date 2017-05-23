c8744b9506c65364a669b777b28904533981f592
Jake Bladt <jake@ambulnz.com>
2017-05-23 17:45:54 -0400

    +main

--- 001/gcd/src/main.rs
+++ 001/gcd/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::Write;
+use std::str::fromStr;
+
+fn main() {
+  let mut numbers = Vec.new();
+  for arg in std::env::args().skip(1) {
+    
+  }
+}
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
   assert!(n != 0 && m != 0);
   while m != 0 {
