cdea66b693306e4fe375d0552bf24f463fce1ed4
Jake Bladt <jake@ambulnz.com>
2017-05-23 17:51:18 -0400

    fix brackets

--- 001/gcd/src/main.rs
+++ 001/gcd/src/main.rs
@@ -5,11 +5,15 @@ fn main() {
   let mut numbers = Vec.new();
   for arg in std::env::args().skip(1) {
     numbers.push(u64:from_str(&arg).expect("error parsing argument"));
-    if numbers.len == 0 {
-      writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
-      std::process::exit(1);
-    }
   }
+
+  if numbers.len == 0 {
+    writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+    std::process::exit(1);
+  }
+
+  
+
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
